e34218ac33129371e932ffc3f7c6a426741f57af
Keiji, Yoshimi <walf443@gmail.com>
2011-06-02 10:29:09 +0900

    Test::Moreのバージョンあげて多少モダンな書き方にしておいた

--- perl/in_time.pl
+++ perl/in_time.pl
@@ -22,20 +22,24 @@ use strict;
 use warnings;
 use Test::More;
 
-plan 'no_plan';
+subtest 'normal case' => sub {
+    ok(!InTime::in_time(9, from => 10, to => 20) , "before start time should not be in time.");
+    ok(InTime::in_time(10, from => 10, to => 20), "start time should be in time.");
+    ok(InTime::in_time(19, from => 10, to => 20), "before end time should be in time.");
+    ok(!InTime::in_time(20, from => 10, to => 20), "before end time should not be in time.");
+};
 
-# normal case test
-ok(!InTime::in_time(9, from => 10, to => 20) , "before start time should not be in time.");
-ok(InTime::in_time(10, from => 10, to => 20), "start time should be in time.");
-ok(InTime::in_time(19, from => 10, to => 20), "before end time should be in time.");
-ok(!InTime::in_time(20, from => 10, to => 20), "before end time should not be in time.");
+subtest 'reverse order case' => sub {
+    ok(!InTime::in_time(19, from => 20, to => 10), "before start time should not be in time ( reverse )");
+    ok(InTime::in_time(20, from => 20, to => 10), "start time should be in time ( reverse )");
+    ok(InTime::in_time(9, from => 20, to => 10), "before end time should be in time ( reverse )");
+    ok(!InTime::in_time(10, from => 20, to => 10), "end time should not be in time ( reverse )");
+};
 
-# reverse order test
-ok(!InTime::in_time(19, from => 20, to => 10), "before start time should not be in time ( reverse )");
-ok(InTime::in_time(20, from => 20, to => 10), "start time should be in time ( reverse )");
-ok(InTime::in_time(9, from => 20, to => 10), "before end time should be in time ( reverse )");
-ok(!InTime::in_time(10, from => 20, to => 10), "end time should not be in time ( reverse )");
+subtest 'in case from time equal to time' => sub {
+    ok(InTime::in_time(10, from => 10, to => 10), "in case from time equal to time");
+    ok(!InTime::in_time(9, from => 10, to => 10), "in case from time equal to time");
+    ok(!InTime::in_time(11, from => 10, to => 10), "in case from time equal to time");
+};
 
-ok(InTime::in_time(10, from => 10, to => 10), "in case from time equal to time");
-ok(!InTime::in_time(9, from => 10, to => 10), "in case from time equal to time");
-ok(!InTime::in_time(11, from => 10, to => 10), "in case from time equal to time");
+done_testing;
